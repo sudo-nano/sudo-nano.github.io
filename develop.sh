906a4d65a764b779987794d0161997b0a62c6223
sudo-nano <cwg1231@gmail.com>
2025-09-11 23:27:26 -0700

    Added future flag to dev script

--- develop.sh
+++ develop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-BUNDLE_GEMFILE="no_dark_visitors_configs/Gemfile_noDV" bundle exec jekyll serve --config _config_no_darkvsitors.yml --drafts
+BUNDLE_GEMFILE="no_dark_visitors_configs/Gemfile_noDV" bundle exec jekyll serve --config _config_no_darkvsitors.yml --drafts --future
